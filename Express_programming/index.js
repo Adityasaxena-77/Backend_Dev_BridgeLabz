af10b7e95ed8663b022a2299be6e9b614d209852
Aditya Saxena <adityasaxena8791@gmail.com>
2026-01-30 15:19:14 +0530

    add these files

---
+++ Express_programming/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>");
+});
+
+app.get("/users", (req, res) => {
+    res.send("<h1>This is users page</h1>");
+});
+
+app.get("/users/:id", (req, res) => {
+    const userId = req.params.id;
+    res.send(`<h1>You are requesting for user ${userId}</h1>`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
